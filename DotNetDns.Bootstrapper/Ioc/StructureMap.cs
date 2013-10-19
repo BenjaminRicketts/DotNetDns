2df8c7d4ac30d9d35f8d3cceefabd675e5c3cc73
Benjamin Ricketts <ben.ricketts@delayedcommuter.co.uk>
2013-10-19 13:52:53 +0100

    Adding StructureMap bootstrapper.

---
+++ DotNetDns.Bootstrapper/Ioc/StructureMap.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using StructureMap;
+using StructureMap.Graph;
+
+namespace DotNetDns.Bootstrapper.Ioc
+{
+    public class StructureMap
+    {
+        public static void Bootstrap()
+        {
+            new StructureMap().Initialise();
+        }
+
+        public void Initialise()
+        {
+            ObjectFactory.Initialize(factory =>
+            {
+                factory.Scan(scanner =>
+                {
+                    scanner.WithDefaultConventions();
+                    ScanSolutionAssemblies(scanner);
+                });
+            });
+
+            AssertConfigurationIsValid();
+        }
+
+        [Conditional("DEBUG")]
+        private void AssertConfigurationIsValid()
+        {
+            ObjectFactory.AssertConfigurationIsValid();
+        }
+
+        private void ScanSolutionAssemblies(IAssemblyScanner scanner)
+        {
+            AppDomain
+                .CurrentDomain
+                .GetAssemblies()
+                .Where(assembly => assembly.FullName.StartsWith("DotNetDns."))
+                .ToList()
+                .ForEach(assembly => scanner.Assembly(assembly));
+        }
+    }
+}
