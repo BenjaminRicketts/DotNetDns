22d34d84a284e95050d23fffeba4fbe4812ac62f
Benjamin Ricketts <ben.ricketts@delayedcommuter.co.uk>
2013-10-21 09:37:32 +0100

    Adding common test helpers project. Adding required test frameworks for DNS service.

---
+++ DotNetDns.TestHelpers/Attributes/IntegrationTestsAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace DotNetDns.TestHelpers.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    public class IntegrationTestsAttribute : CategoryAttribute
+    {
+        public IntegrationTestsAttribute()
+            : base("Integration")
+        {
+        }
+    }
+}
