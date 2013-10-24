18d03d725a2d6f7cc9d8daabaa76593fc44ffa59
Benjamin Ricketts <ben.ricketts@delayedcommuter.co.uk>
2013-10-24 08:36:14 +0100

    Validating against null settings being passed to UdpListener.

---
+++ DotNetDns.Server.Tests/Server/Listeners/UdpListenerTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using DotNetDns.Server.Server.Listeners;
+using NUnit.Framework;
+
+namespace DotNetDns.Server.Tests.Server.Listeners
+{
+    [TestFixture]
+    public class UdpListenerTests
+    {
+        [Test]
+        [ExpectedException(typeof(Exception), ExpectedMessage = "No DNS server settings have been found.")]
+        public void Constructing_With_No_Settings_Throws()
+        {
+            new UdpListener(null);
+        }
+    }
+}
--- DotNetDns.Server/Server/Listeners/UdpListener.cs
+++ DotNetDns.Server/Server/Listeners/UdpListener.cs
@@ -1,12 +1,29 @@
 ﻿using System;
+using System.Net;
+using DotNetDns.Server.Settings;
 
 namespace DotNetDns.Server.Server.Listeners
 {
     public class UdpListener : IDnsListener
     {
+        private readonly IDnsServerSettings _settings;
+
+        public UdpListener(IDnsServerSettings settings)
+        {
+            _settings = settings;
+
+            ValidateSettings();
+        }
+
         public void StartListening()
         {
             throw new NotImplementedException();
+        }
+
+        private void ValidateSettings()
+        {
+            if (_settings == null)
+                throw new Exception("No DNS server settings have been found.");
         }
     }
 }
