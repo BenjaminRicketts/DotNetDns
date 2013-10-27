b15c99692ecd8807a562d218a854085770f6686a
Benjamin Ricketts <ben.ricketts@delayedcommuter.co.uk>
2013-10-27 10:32:14 +0000

    Adding domain name validation.

---
+++ DotNetDns.Common/DomainNames/DomainNameValidator.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Linq;
+
+namespace DotNetDns.Common.DomainNames
+{
+    public class DomainNameValidator
+    {
+        private const int MaxDomainNameLength = 253;
+        private const int MaxLabelLength = 63;
+
+        private readonly ITopLevelDomains _topLevelDomains;
+
+        public DomainNameValidator(ITopLevelDomains topLevelDomains)
+        {
+            _topLevelDomains = topLevelDomains;
+        }
+
+        public void ValidateDomainName(string domainName)
+        {
+            ThrowIfTopLevelDomainIsInvalid(
+                ThrowIfDomainNameIsTooLong(
+                    ThrowIfLabelIsTooLong(
+                        ThrowIfNullOrEmpty(domainName))));
+        }
+
+        private void Throw(string message)
+        {
+            throw new Exception(message);
+        }
+
+        private string ThrowIfDomainNameIsTooLong(string domainName)
+        {
+            if (domainName.Length > MaxDomainNameLength)
+                Throw(string.Format("The domain name '{0}' is too long. Domain names may only be upto 253 characters in length.", domainName));
+
+            return domainName;
+        }
+
+        private string ThrowIfLabelIsTooLong(string domainName)
+        {
+            foreach (var label in domainName.Split('.'))
+            {
+                if (label.Length > MaxLabelLength)
+                    Throw(string.Format("The label '{0}' is too long. Labels may only be upto 63 characters in length.", label));
+            }
+
+            return domainName;
+        }
+
+        private string ThrowIfNullOrEmpty(string domainName)
+        {
+            if (string.IsNullOrWhiteSpace(domainName))
+                Throw("A domain name cannot be null or empty.");
+
+            return domainName;
+        }
+
+        private string ThrowIfTopLevelDomainIsInvalid(string domainName)
+        {
+            var labels = domainName.Split('.').ToList();
+
+            if (labels.Count > 0)
+            {
+                var topLevelDomain = labels.Last();
+
+                if (_topLevelDomains.IsNotKnown(topLevelDomain))
+                    Throw(string.Format("The top level domain '{0}' is invalid.", topLevelDomain));
+            }
+
+            return domainName;
+        }
+    }
+}
---
+++ DotNetDns.Common/DomainNames/ITopLevelDomains.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace DotNetDns.Common.DomainNames
+{
+    public interface ITopLevelDomains
+    {
+        IList<string> GetAll();
+        bool IsNotKnown(string topLevelDomain);
+    }
+}
